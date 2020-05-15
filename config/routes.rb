86502ed3c986c58afecc87d8450646518dd7ef20
vagrant <vagrant@localhost.localdomain>
2020-05-15 13:01:57 +0000

    [Update]add refile

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
         sessions: 'users/sessions',
         registrations: 'users/registrations'
     }
+    resources :users,only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
