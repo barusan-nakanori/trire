8f891025e5f7b593c94772f039046a6d24bae1ea
vagrant <vagrant@localhost.localdomain>
2020-05-16 09:24:32 +0000

    [Update]authenticate_user

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
         registrations: 'users/registrations'
     }
     resources :users,only: [:index,:show,:edit,:update]
+    resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
