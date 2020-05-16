7e5dca7e76aa26c7dfbab89804b5e1043436d654
vagrant <vagrant@localhost.localdomain>
2020-05-16 07:09:38 +0000

    [Add]Book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_121556) do
+ActiveRecord::Schema.define(version: 2020_05_16_070540) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "title"
+    t.text "body"
+    t.string "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
