dedbcd9a4427847d0527de455c59f958dc69b9e8
vagrant <vagrant@localhost.localdomain>
2020-05-16 08:06:01 +0000

    [Fix]validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :books, dependant: :destroy
-  validetes :name,presence: true,length: {minimum: 2 ,maximum: 20}
-  validetes :email,presence: true
+  has_many :books, dependent: :destroy
+  validates :name,presence: true,length: {minimum: 2 ,maximum: 20}
+  validates :email,presence: true
+  validates :introduction,length: {maximum: 50}
 end
