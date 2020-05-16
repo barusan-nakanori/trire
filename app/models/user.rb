53dd2335f986fda43e8b44ba7f17c8a945f5fba0
vagrant <vagrant@localhost.localdomain>
2020-05-16 07:33:16 +0000

    [Add]add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books, dependant: :destroy
+  validetes :name,presence: true,length: {minimum: 2 ,maximum: 20}
+  validetes :email,presence: true
 end
