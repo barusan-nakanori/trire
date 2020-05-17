b58cd4b3666d01aa2f6c36346f631e04a0586614
vagrant <vagrant@localhost.localdomain>
2020-05-17 05:36:30 +0000

    [Fix]ログイン機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
   def index
   	@users = User.all
+  	@book = Book.new
   end
 
   def show
   	@user = User.find(params[:id])
+  	@book = Book.new
   end
 
   def edit
