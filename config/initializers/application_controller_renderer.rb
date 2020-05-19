bffdb31db025b70af5ca5819267fbbd166d43c50
vagrant <vagrant@localhost.localdomain>
2020-05-19 11:54:05 +0000

    [Add]画像からのリンク

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0acd5f56478bfe22d0647371fdf16154b221d86793684b73814e627762a20f560420ba1af8674335debcbf7df438767c72c4646c9e69292d6fe90379f5d8f7cc'
