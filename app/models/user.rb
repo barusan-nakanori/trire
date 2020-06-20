fb0a5875173e9548332e9213387f31df2c4e7f07
vagrant <nori.work.vtr250@gmail.com>
2020-06-20 10:09:32 +0000

    add create-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
   has_many :comments
+  has_many :follower, class_name:"Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :followed, class_name:"Relationship",
+            foreign_key: "followed_id",
+            dependent: :destroy
   attachment :profile_image
   validates :name,presence: true,length: {minimum: 2 ,maximum: 20}
   validates :email,presence: true
