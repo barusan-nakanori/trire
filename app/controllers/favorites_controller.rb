480cdd69f85ddf6ad9aa3a685f04707e72f04787
vagrant <nori.work.vtr250@gmail.com>
2020-06-30 12:15:24 +0000

    finish favorite-ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,13 @@ class FavoritesController < ApplicationController
 
 	 def create
 	    @favorite = current_user.favorites.create(book_id: params[:book_id])
-	    # redirect_back(fallback_location: root_path)
+	    redirect_back(fallback_location: root_path)
 	 end
 
 	 def destroy
-	    @favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
+	 	@book = Book.find(params[:id])
+	    @favorite = current_user.favorites.find_by(book_id: @book.id)
 	    @favorite.destroy
-	    # redirect_back(fallback_location: root_path)
+	    redirect_back(fallback_location: root_path)
   	end
 end
